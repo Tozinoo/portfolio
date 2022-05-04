fe912b8db3bb778107bf9cd9dcdcdbae5c482829
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-05-04 10:28:34 +0900

    Feat: Add build

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
 const SocialMedia = () => (
     <div className="app__social">
